6c463ec4d9deee6060417467efb56e2054986fa5
Muhammad Naeem <naeem0313@gmail.com>
2020-08-15 10:58:13 +0500

    get Employee List

--- app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/viewmodel/CommonViewModel.kt
+++ app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/viewmodel/CommonViewModel.kt
@@ -1,4 +1,18 @@
 package com.muhammadnaeem.kotlinsqlite_demo.viewmodel
 
-class CommonViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.muhammadnaeem.kotlinsqlite_demo.model.EmployeeModel
+import com.muhammadnaeem.kotlinsqlite_demo.repo.DbRepository
+
+class CommonViewModel (application: Application) : AndroidViewModel(application) {
+    private val mRepository: DbRepository = DbRepository(application.applicationContext)
+
+    //get List of employee from DB
+    fun getAll_employee(): MutableLiveData<List<EmployeeModel>>? {
+        return mRepository.getAll_employee()
+    }
+
+
 }
