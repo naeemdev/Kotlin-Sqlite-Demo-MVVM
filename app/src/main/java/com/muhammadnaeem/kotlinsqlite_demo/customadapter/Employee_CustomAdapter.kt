5cb89a8cfaef67a36a68b7f48192ba691f9a02d1
Muhammad Naeem <naeem0313@gmail.com>
2020-08-15 11:05:54 +0500

    Done: Employee CustomAdapter

--- app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/customadapter/Employee_CustomAdapter.kt
+++ app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/customadapter/Employee_CustomAdapter.kt
@@ -1,4 +1,69 @@
 package com.muhammadnaeem.kotlinsqlite_demo.customadapter
 
-class Employee_CustomAdapter {
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.muhammadnaeem.kotlinsqlite_demo.R
+import com.muhammadnaeem.kotlinsqlite_demo.databinding.ItemEmployeeBinding
+import com.muhammadnaeem.kotlinsqlite_demo.interfaceListner.ItemClickListner
+import com.muhammadnaeem.kotlinsqlite_demo.model.EmployeeModel
+
+
+class Employee_CustomAdapter ( var clickListener: ItemClickListner? = null):
+    RecyclerView.Adapter<Employee_CustomAdapter.EmployeeViewHolder>() {
+
+    private var mEmployeeModellist: ArrayList<EmployeeModel>? = null
+
+      override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): EmployeeViewHolder {
+        val mItemIconBinding: ItemEmployeeBinding =
+            DataBindingUtil.inflate(
+                LayoutInflater.from(viewGroup.context),
+                R.layout.item_employee, viewGroup, false
+            )
+        return EmployeeViewHolder(mItemIconBinding)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onBindViewHolder(
+        mEmployeeViewHolder: EmployeeViewHolder,
+        i: Int
+    ) {
+
+        val currentitem = mEmployeeModellist!![i]
+
+        mEmployeeViewHolder.mItemIconBinding.item=currentitem
+        mEmployeeViewHolder.mItemIconBinding.cvRoot.setOnClickListener(View.OnClickListener {
+            clickListener!!.onClick(currentitem.id,mEmployeeViewHolder.adapterPosition)
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return if (mEmployeeModellist != null) {
+            mEmployeeModellist!!.size
+        } else {
+            0
+        }
+    }
+
+    fun setitemList(local_list: ArrayList<EmployeeModel>?) {
+        this.mEmployeeModellist = local_list
+
+        notifyDataSetChanged()
+    }
+
+    fun removeItem(position: Int) {
+        mEmployeeModellist?.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+
+    inner class EmployeeViewHolder(var mItemIconBinding: ItemEmployeeBinding) :
+        RecyclerView.ViewHolder(mItemIconBinding.root)
+
+
+
 }
