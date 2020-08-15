0b6ff1702f4aec33eebe386b2c23238ff702f5f3
Muhammad Naeem <naeem0313@gmail.com>
2020-08-15 10:33:32 +0500

    completed Second screen design

--- app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/SecondFragment.kt
+++ app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/SecondFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.findNavController
 import com.muhammadnaeem.kotlinsqlite_demo.databinding.FragmentSecondBinding
 
 /**
@@ -30,8 +28,6 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+
     }
 }
