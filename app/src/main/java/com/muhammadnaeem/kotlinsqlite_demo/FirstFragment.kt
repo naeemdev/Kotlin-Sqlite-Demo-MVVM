d962e4d32c5d617505eb81ec29afacc17718b91c
Muhammad Naeem <naeem0313@gmail.com>
2020-08-15 09:47:59 +0500

    Bind the FirstFragment.kt

--- app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/FirstFragment.kt
+++ app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/FirstFragment.kt
@@ -6,19 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.muhammadnaeem.kotlinsqlite_demo.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    var binding: FragmentFirstBinding?=null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_first, container, false)
+        binding!!.lifecycleOwner = viewLifecycleOwner
+        return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
