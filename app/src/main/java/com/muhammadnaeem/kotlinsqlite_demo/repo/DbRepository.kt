688a732114e4b37c49f184e20a9a4c013633ca77
Muhammad Naeem <naeem0313@gmail.com>
2020-08-15 16:48:55 +0500

    Get employee Roles List based on Employee ID

--- app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/repo/DbRepository.kt
+++ app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/repo/DbRepository.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import com.muhammadnaeem.kotlinsqlite_demo.dbhelper.DbHelper
 import com.muhammadnaeem.kotlinsqlite_demo.model.EmployeeModel
+import com.muhammadnaeem.kotlinsqlite_demo.model.RolesModel
 import com.muhammadnaeem.kotlinsqlite_demo.utils.*
 import java.util.ArrayList
 
@@ -107,6 +108,44 @@ class DbRepository(private val context: Context)  {
         }
 
         return mEmployeeModel_live
+    }
+
+    //get Employee Roles
+    fun getAll_employeeroles(emp_id:Int):  MutableLiveData<List<RolesModel>> {
+
+        val appDbHelper = DbHelper.getInstance(context)
+        val employeerolelist = MutableLiveData<List<RolesModel>>()
+        try {
+
+            appDbHelper.readableDatabase.use { db ->
+                db.query(
+                    TABLE_ROLES, null,
+                    "$COLUMN_EMP_ID = ? ", arrayOf(emp_id.toString()), null, null, null
+                ).use { cursor ->
+
+                    if (cursor != null)
+                        if (cursor.moveToFirst()) {
+                            val mRolesModellist = ArrayList<RolesModel>()
+                            do {
+                                var mRolesModel= RolesModel()
+
+                                mRolesModel.name= cursor.getString(cursor.getColumnIndex(
+                                    COLUMN_ROLES_NAME
+                                ))
+
+                                mRolesModellist.add(mRolesModel)
+
+                            } while (cursor.moveToNext())
+                            employeerolelist.postValue(mRolesModellist)
+                            return employeerolelist
+                        }
+                }
+            }
+        } catch (e: Exception) {
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+
+        return employeerolelist
     }
 
 }
