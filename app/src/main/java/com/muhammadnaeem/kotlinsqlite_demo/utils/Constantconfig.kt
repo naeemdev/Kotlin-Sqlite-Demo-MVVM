4b3d69b2b4a249dc2201c0bba100248dd2146e26
Muhammad Naeem <naeem0313@gmail.com>
2020-08-15 10:37:46 +0500

    Define table name and COLUMN name

---
+++ app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/utils/Constantconfig.kt
@@ -0,0 +1,17 @@
+package com.muhammadnaeem.kotlinsqlite_demo.utils
+
+//employee table
+
+const val TABLE_EMPLOYEE = "tbl_employee"
+const val COLUMN_ID = "id"
+const val COLUMN_EMP_FIRSTNAME = "emp_firstname"
+const val COLUMN_EMP_LASTNAME = "emp_lastname"
+const val COLUMN_EMP_EMAIL = "emp_email"
+const val COLUMN_EMP_PASSWORD = "emp_password"
+const val COLUMN_EMP_DESG = "emp_desg"
+const val COLUMN_EMP_CODE = "emp_code"
+
+//Roles table for employee
+const val TABLE_ROLES = "tbl_emp_roles"
+const val COLUMN_EMP_ID = "emp_id"
+const val COLUMN_ROLES_NAME = "emp_rolename"
