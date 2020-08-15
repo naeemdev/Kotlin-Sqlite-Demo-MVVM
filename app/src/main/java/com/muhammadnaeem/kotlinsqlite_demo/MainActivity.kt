430f44fd43f835494d78c429cbf153fb468f9056
Muhammad Naeem <naeem0313@gmail.com>
2020-08-15 09:42:36 +0500

    Bind the MainActivity Xml

--- app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/MainActivity.kt
+++ app/src/main/java/com/muhammadnaeem/kotlinsqlite_demo/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.muhammadnaeem.kotlinsqlite_demo
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.muhammadnaeem.kotlinsqlite_demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
     }
 
 
